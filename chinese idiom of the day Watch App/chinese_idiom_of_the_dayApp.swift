26a0efe133ec6199c1248af821682dbf23fa85c6
AkaCoder404 <akacoder404@outlook.com>
2024-11-24 17:51:41 +0800

    Initial Commit

---
+++ chinese idiom of the day Watch App/chinese_idiom_of_the_dayApp.swift
@@ -0,0 +1,17 @@
+//
+//  chinese_idiom_of_the_dayApp.swift
+//  chinese idiom of the day Watch App
+//
+//  Created by George Li on 11/24/24.
+//
+
+import SwiftUI
+
+@main
+struct chinese_idiom_of_the_day_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
